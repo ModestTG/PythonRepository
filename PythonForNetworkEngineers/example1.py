65fa967a3cb64f1a5e7a4f36bc3df03fa05f1da8
ModestTG <elliotweishaar27@gmail.com>
2018-11-06 19:00:06 -0600

    Added Comments

--- PythonForNetworkEngineers/example1.py
+++ PythonForNetworkEngineers/example1.py
@@ -5,8 +5,8 @@ from __future__ import print_function
 ip_addr1 = '8.8.8.8'
 print(ip_addr1)
 try:
-    ip_addr2 = raw_input("Enter an IP Address: ")
+    ip_addr2 = raw_input("Enter an IP Address: ") #python2 input function
 except NameError:
-    ip_addr2 = input("Enter an IP Address: ")
+    ip_addr2 = input("Enter an IP Address: ") #python3 input funtion
 print(ip_addr2)
 
