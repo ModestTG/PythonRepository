21d4b699130535304e6c676eaf121591dba63524
ModestTG <elliotweishaar27@gmail.com>
2018-11-06 22:22:20 -0600

    Completed Strings

--- PythonForNetworkEngineers/example1.py
+++ PythonForNetworkEngineers/example1.py
@@ -1,6 +1,7 @@
-#allows print() funtion to work in python2
-from __future__ import print_function
+#allows print() funtion and bytestrings to work in python2
+from __future__ import print_function, unicode_literals
 
+my_str = 'whatever'
 
 ip_addr1 = '8.8.8.8'
 print(ip_addr1)
