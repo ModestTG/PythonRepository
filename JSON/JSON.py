5bbceab4f8163be496f5f2d70caee29f8689e7d4
Elliot Weishaar <elliotweishaar27@gmail.com>
2018-11-07 08:31:16 -0600

    Fixed Errors

--- JSON/JSON.py
+++ JSON/JSON.py
@@ -1,5 +1,5 @@
 import json
 import sys
-json_string = open("D:\AllSetsArray.json", encoding="utf8", errors='ignore')
+json_string = open(r"D:\AllSetsArray.json", encoding="utf8", errors='ignore')
 parsed_json = json.load(json_string)
 print(parsed_json["name"])
