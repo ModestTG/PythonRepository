45ca9d8bc25f8aa1253429c774a5486a9086bfae
ModestTG <elliotweishaar27@gmail.com>
2018-11-08 21:14:18 -0600

    Epoch conversion works

--- EpochTimeConverter/epoch.py
+++ EpochTimeConverter/epoch.py
@@ -7,11 +7,13 @@ try:
 except NameError:
     date_time = input("Input your Date (MM/DD/YYYY hh:mm:ss): ")
 
-date_time_dmy = date_time.split(" ")[0].split("/")
-date_time_hms = date_time.split(" ")[1].split(":")
+date_time_dmy = tuple(map(int, date_time.split(" ")[0].split("/")))
+date_time_hms = tuple(map(int, date_time.split(" ")[1].split(":")))
 print(f"{date_time_dmy} {date_time_hms}")
 
-epochTime = time.mktime()
+time_tuple = (date_time_dmy[2], date_time_dmy[1], date_time_dmy[0], date_time_hms[0], date_time_hms[1], date_time_hms[2], 0, 0, -1)
+# print(len(time_tuple))
+epochTime = time.mktime(time_tuple)
 print(epochTime)
 # epochTime = time.gmtime(1541707494)
 # print(epochTime)
