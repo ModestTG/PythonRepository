3db06f9b5ac3e242128ccddd3a41c58360dec178
Elliot Weishaar <elliotweishaar27@gmail.com>
2018-11-13 11:38:33 -0600

    Updated Syntax

--- Spiral/Spiral.py
+++ Spiral/Spiral.py
@@ -12,12 +12,12 @@ def spiral(n):
             return spiral_part(x+1, y, n) + 1
         if x < (n-y) and y > x:
             return spiral_part(x, y-1, n) - 1
- 
+
     array = [[0] * n for j in range(n)]
     for x in range(n):
         for y in range(n):
             array[x][y] = spiral_part(y, x, n)
     return array
- 
+
 for row in spiral(5):
-    print (" ".join("%2s" % x for x in row))
+    print(" ".join("%2s" % x for x in row))
