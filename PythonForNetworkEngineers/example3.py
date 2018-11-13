4a16a6d1ada8a99fb7da04f1761cd14d3a20dd08
Elliot Weishaar <elliotweishaar27@gmail.com>
2018-11-13 11:48:20 -0600

    updated comments

--- PythonForNetworkEngineers/example3.py
+++ PythonForNetworkEngineers/example3.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, unicode_literals, division
 
 # This is considered the proper way to open a file, as you do not need to include the f.close() syntax in this block
+# The 'r' prepending the file path makes this a "raw" string. This string ignores all escape characters ('\')
 with open(r"D:\Code\Corporea\txt\declarationofindependance.txt") as f:
     output = f.read()
 
