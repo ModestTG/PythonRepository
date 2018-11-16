c6ed46e3cc709a19bd2cc78085fa69c33046e4c9
Elliot Weishaar <elliotweishaar27@gmail.com>
2018-11-16 15:48:29 -0600

    Updated Code Sytle

--- EpochTimeConverter/epoch.py
+++ EpochTimeConverter/epoch.py
@@ -10,6 +10,7 @@ except NameError:
 date_time_dmy = tuple(map(int, date_time.split(" ")[0].split("/")))
 date_time_hms = tuple(map(int, date_time.split(" ")[1].split(":")))
 
-time_tuple = (date_time_dmy[2], date_time_dmy[1], date_time_dmy[0], date_time_hms[0], date_time_hms[1], date_time_hms[2], 0, 0, -1)
+time_tuple = (date_time_dmy[2], date_time_dmy[1], date_time_dmy[0], date_time_hms[0], date_time_hms[1],
+              date_time_hms[2], 0, 0, -1)
 epochTime = time.mktime(time_tuple)
 print(int(epochTime))
