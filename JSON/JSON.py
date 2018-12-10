577e2409a6c0104ac7ea4cee408f120d11690c62
Elliot Weishaar <elliotweishaar27@gmail.com>
2018-12-10 13:13:30 -0600

    Fixed Open File format

--- JSON/JSON.py
+++ JSON/JSON.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import json
 import sys
-json_string = open(r"D:\AllSetsArray.json", encoding="utf8", errors='ignore')
-parsed_json = json.load(json_string)
+with open(r"D:\AllSetsArray.json", encoding="utf8", errors='ignore') as json_string:
+    parsed_json = json.load(json_string)
+
 print(parsed_json["name"])
